c18a89ae3236eb30534987a0e8885bfdf5dc7fbc
hiropon-github <hiropon0628@outlook.com>
2020-01-03 17:52:54 +0900

    First Commit

---
+++ app/src/main/java/com/morghenlethe/hplayer/MainActivity.java
@@ -0,0 +1,142 @@
+package com.morghenlethe.hplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button playBtn;
+    private SeekBar positionBar;
+    private SeekBar volumeBar;
+    private TextView elapsedTimeLabel;
+    private TextView remainingTimeLabel;
+    private MediaPlayer mp;
+    private int totalTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        playBtn = findViewById(R.id.playBtn);
+        elapsedTimeLabel = findViewById(R.id.elapsedTimeLabel);
+        remainingTimeLabel = findViewById(R.id.remainingTimeLabel);
+
+        // 初期化
+        mp = MediaPlayer.create(this, R.raw.non);
+        mp.setLooping(true);
+        mp.seekTo(0);
+        mp.setVolume(0.5f, 0.5f);
+        totalTime = mp.getDuration();
+
+        positionBar = findViewById(R.id.positionBar);
+        positionBar.setMax(totalTime);
+        positionBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        if(fromUser) {
+                            mp.seekTo(progress);
+                            positionBar.setProgress(progress);
+                        }
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+        );
+
+        volumeBar = findViewById(R.id.volumeBar);
+        volumeBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        float volumeNum = progress / 100f;
+                        mp.setVolume(volumeNum, volumeNum);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+        );
+
+        // Thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mp != null) {
+                    try {
+                        Message msg = new Message();
+                        msg.what = mp.getCurrentPosition();
+                        handler.sendMessage(msg);
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {}
+                }
+            }
+        }).start();
+    }
+
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            int currentPosition = msg.what;
+
+            // 再生位置更新
+            positionBar.setProgress(currentPosition);
+
+            // 経過時間ラベル更新
+            String elapsedTime = createTimeLabel(currentPosition);
+            elapsedTimeLabel.setText(elapsedTime);
+
+            // 残り時間ラベル更新
+            String remainingTime = "- " + createTimeLabel(totalTime - currentPosition);
+            remainingTimeLabel.setText(remainingTime);
+
+            return true;
+        }
+    });
+
+    public String createTimeLabel(int time) {
+        String timeLabel = "";
+        int min = time / 1000 / 60;
+        int sec = time / 1000 % 60;
+
+        timeLabel = min + ":";
+        if (sec < 10) timeLabel += "0";
+        timeLabel += sec;
+
+        return timeLabel;
+    }
+
+    public void playBtnClick(View view) {
+        if(!mp.isPlaying()) {
+            mp.start();
+            playBtn.setBackgroundResource(R.drawable.stop);
+        } else {
+            mp.pause();
+            playBtn.setBackgroundResource(R.drawable.play);
+        }
+    }
+}
